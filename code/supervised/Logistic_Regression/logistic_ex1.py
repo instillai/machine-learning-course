06d34f12ae82daa423d7e9c25d8c522bd7beac46
Eric Wynn <weric16@vt.edu>
2019-03-28 17:15:53 -0400

    Update logistic_ex1.py

--- code/supervised/Logistic_Regression/logistic_ex1.py
+++ code/supervised/Logistic_Regression/logistic_ex1.py
@@ -1,9 +1,17 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
+
+#Loads the sample dataset iris from sklearn's datasets
 X, y = load_iris(return_X_y=True)
+
+#Creates the Logistic Regression object
 clf = LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial')
+#Fit the model to match the data
 clf = clf.fit(X, y)
-clf.predict(X[:2, :])
-clf.predict_proba(X[:2, :])
-#Now, we check the fit of the model
-clf.score(X, y)
+
+#Now, we show the predictions for each row of the dataset.
+print(clf.predict(X[:2, :]))
+print(clf.predict_proba(X[:2, :]))
+
+#Check the fit of the model.  Score returns the mean accuracy on the data.  
+print(clf.score(X, y))
