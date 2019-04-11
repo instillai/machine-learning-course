a7c1e9024a2bbedb8fd3c9f593b27ff1505affb7
b-sherman <47649014+b-sherman@users.noreply.github.com>
2019-04-11 19:07:37 -0400

    Updated bell curve code to use seaborn

--- code/supervised/Naive_Bayes/bell_curve.py
+++ code/supervised/Naive_Bayes/bell_curve.py
@@ -1,9 +1,11 @@
 import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Create a bell curve plot using numpy and stats
 x = np.linspace(norm.ppf(0.01), norm.ppf(0.99), 100)
+sns.set_style("darkgrid")
 plt.plot(x, norm.pdf(x))
 
 # Remove ticks from the plot
