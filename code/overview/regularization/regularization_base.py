08665efc855d43005f15dd0774a0abe02014e11f
Brendan Sherman <brens97@vt.edu>
2019-04-16 19:12:26 -0400

    Restructured the regularization module
    +Simplified code
    +Changed images
    +Improved text

--- code/overview/regularization/regularization_base.py
+++ code/overview/regularization/regularization_base.py
@@ -1,24 +1,16 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.datasets import make_regression
 import numpy as np
 
-# We will create some x-values and randomly choose some as data points
-X = np.linspace(0, 10, 100)
-# We are fixing the random number seed for consistency
-rn = np.random.RandomState(0)
-# Shuffle the data for variety
-rn.shuffle(X)
-# Grab the first 30 of our shuffled points and sort them for plotting
-X = np.sort(X[:30])
-# Our output will be a quadratic function
-y = X**2
-# We will add some variance to the data so that it's more interesting
-y = y + (((np.random.rand(30) * 2) - 1) * 30)
+# Create a data set for analysis
+x, y = make_regression(n_samples=100, n_features = 1, noise=15, random_state=0)
+y = y ** 2
 
 # Plot data
 sns.set_style("darkgrid")
-plt.scatter(X, y, marker='o')
+plt.scatter(x, y, marker='o')
 plt.xticks(())
 plt.yticks(())
 plt.tight_layout()
