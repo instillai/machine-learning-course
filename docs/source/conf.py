8440f11937b77d1b621145dfebb1002620b7f42e
Amirsina <Amirsina@nwtadmin2018s-MacBook-Pro.local>
2019-07-05 13:22:26 -0400

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ pygments_style = 'sphinx'
 htmlhelp_basename = 'ReadTheDocsdoc'
 latex_documents = [
     (master_doc, 'Machine-Learning-Course.tex', u'Machine-Learning-Course Documentation',
-     u'Amirsina Torfi Brendan Sherman James E Hopkins Zac Smith Eric Wynn', 'manual'),
+     u'Amirsina Torfi \n Brendan Sherman James E Hopkins Zac Smith Eric Wynn', 'manual'),
 ]
 man_pages = [
     (master_doc, 'Machine-Learning-Course', u'Machine-Learning-Course Documentation',
